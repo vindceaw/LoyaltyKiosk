9a3b237a0eb79bfb87e84daf251c95ad1a8b08f8
vindceaw <vindceawth@gmail.com>
2021-07-15 00:33:42 +0800

    mmebers added

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 'use strict';
 
+// creating class for new members
+class newAuthMembers {
+  constructor ()
+}
+
+// creating object to save members details
+const authMembers ={
+  auth00001 : { 
+    emailAdd: 'vindce@gmail.com',
+    password: 'Va1234567',
+    freeGame: 1,
+    earnedGame: 4,
+    gender: 'male',
+    cardStatus: 'Premier',
+    statusPts: 50
+  }
+  auth00002 : {
+    emailAdd: 'jasonttt@gmail.com',
+    password: 'jttt123',
+    freeGame: 1,
+    earnedGame: 2,
+    gender: 'male',
+    cardStatus: 'Premier',
+    statusPts: 50
+  }
+}
