58a074e4d004a3dd4ea9ecca829f86e88948e981
vindceaw <vindceawth@gmail.com>
2021-07-17 11:17:01 +0800

    listening to button-login click

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 'use strict';
 
-// creating class for new members
-class newAuthMembers {
-  constructor ()
-}
+
 
 // creating object to save members details
 const authMembers ={
-  auth00001 : { 
+  auth00001:{ 
     emailAdd: 'vindce@gmail.com',
     password: 'Va1234567',
     freeGame: 1,
@@ -15,8 +12,9 @@ const authMembers ={
     gender: 'male',
     cardStatus: 'Premier',
     statusPts: 50
-  }
-  auth00002 : {
+  },
+
+  auth00002:{
     emailAdd: 'jasonttt@gmail.com',
     password: 'jttt123',
     freeGame: 1,
@@ -25,4 +23,29 @@ const authMembers ={
     cardStatus: 'Premier',
     statusPts: 50
   }
-}
+};
+
+//checking the credentials of authMembers
+//const authMembersNum= Object.keys(authMembers).length;
+const emailAddEntered = document.getElementById('defaultForm-email').innerText;
+
+const checkLogin = () => {
+  for (const member in authMembers){
+    if ( emailAddEntered === authMembers[member].emailAdd){
+      
+      console.log('vindce you made it');
+      console.log(emailAddEntered)
+    } else {
+      //console.log(authMembers[member].emailAdd);
+      console.log('jialat liao');
+      console.log(emailAddEntered);
+      document.getElementById('defaultForm-email').innerText="fuck you";
+    }
+  }
+};
+//TESTING MODE
+  // emailAddEntered='jasonttt@gmail.com'
+  // checkLogin(emailAddEntered);
+document.getElementById('btn-ModalLogin').addEventListener('click', function (){
+  console.log(emailAddEntered);
+});
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const checklogin = (emailAdd, password) => {
+//   for (let i=0; i< authMembers.length)
+
+// }
+
+// document.getElementById('btn-login').addEventListener('click', alert('hello'))
