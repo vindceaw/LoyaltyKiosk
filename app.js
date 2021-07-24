ef759a3d3cb3f2dc66df68752624a037b2eae32f
vindceaw <vindceawth@gmail.com>
2021-07-24 08:13:58 +0800

    completed

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const authMembers ={
     memberName: 'Vindce Aw Tong Howe',
     emailAdd: 'v',
     password: 'v',
-    earnedGame: 4,
+    freeGame: 1,
     earnedDrawChances: 10,
     gender: 'male',
-    cardStatus: 'Premier',
+    cardStatus: 'Platinum Member',
     statusPts: 1500
   },
 
@@ -17,37 +17,53 @@ const authMembers ={
     memberName: 'Jason Tan Tik Tok',
     emailAdd: 'jasonttt@gmail.com',
     password: 'jttt123',
-    earnedGame: 2,
+    freeGame: 1,
     earnedDrawChances: 1,
     gender: 'male',
-    cardStatus: 'Premier',
-    statusPts: 50
+    cardStatus: 'Premier Member',
+    statusPts: 150
   }
 };
 
-//disable back button
+// array of prizes
+const prizes = {
+  firstPrize :{
+    prizeID: [1],
+    prizeInfo: "$50,000 Cash",
+    qty: 1
+  },
 
-// function preventBack() {
-//   window.history.forward();
-// }
-//checking the credentials of authMembers
-//const authMembersNum= Object.keys(authMembers).length;
-// console.log(emailAddEntered);
-// Get weather
+  secondPrize :{
+    prizeID: [2,3,4],
+    prizeInfo: "$5,000 Cash",
+    qty: 3
+  },
+  thirdPrize :{
+    prizeID: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+    prizeInfo: "$1,000 Cash",
+    qty: 16
+  },
+  forthprize :{
+    prizeID: [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40],
+    prizeInfo: "$100 Cash",
+    qty: 20
+  },
+}
 
-const getCountryWeatherAsync = async () => {
-  try{
-  const url = `https://goweather.herokuapp.com/weather/Singapore`
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
-  } catch(err) {
-  console.error(err);
-  } 
+
+
+// API to show weather before booking pool***************************************************
+const getCurrent = async ()=> {
+  const myKey ="194a5aff8380bcec0960801dc24dce77";
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${myKey}`);
+
+  const data = await response.json();
+  console.log(data)
+  document.getElementById('poolMsg').innerText = `${Math.round(data.main.temp-273.15)} degree Celsius | ${data.weather[0].description}`;
 };
-console.log(getCountryWeatherAsync());
 
-//Profile Modal to display information
+
+//function to check login details and display member page
 
 
 
@@ -61,15 +77,18 @@ const checkLogin = () => {
         $('#modalLoginForm').modal('hide');
         $('#modalProfile').modal('show');
         // show member status
-        document.getElementById('memberStatus').innerText = `${authMembers[member].memberName} | ${authMembers[member].cardStatus}`;
+        document.getElementById('memberName').innerText = authMembers[member].memberName;
+        document.getElementById('memberStatus').innerText = authMembers[member].cardStatus;
+
         // show weather for pool
-        getCountryWeatherAsync();
+        getCurrent();
+        
         // show draw chances
         document.getElementById('drawMsg').innerText=`${authMembers[member].earnedDrawChances} draw chances Activate Now!`;
       
         //show game play
 
-        document.getElementById('gameMsg').innerText=`${authMembers[member].earnedGame + 1} Game Play(s)`;
+        document.getElementById('gameMsg').innerText=`${authMembers[member].freeGame} Game Play(s)`;
 
         // window.location.assign("/profile.html"); 
         // const profileStatus = document.getElementById('profileStatus');
@@ -88,9 +107,73 @@ const checkLogin = () => {
 };
 
 
-//TESTING MODE
-  // emailAddEntered='jasonttt@gmail.com'
-  // checkLogin(emailAddEntered);
+//login button onclick
 document.getElementById('btn-ModalLogin').addEventListener('click', checkLogin);
 
+//draw button onclick
+document.getElementById('draw-btn').addEventListener('click',()=> {
+  //document.getElementById('msgDisplay').innerText="You have activated all your drawing entries."
+  document.getElementById('drawMsg').style.fontWeight='bold';
+  document.getElementById('drawMsg').style.color = 'red';
+  document.getElementById('drawMsg').innerText=`Draw entries ACTIVATED`;
+  document.getElementById('draw-btn').disabled = 'true';
+  
+});
 
+//game button onclick
+  //function to create game table
+  const createTable = () => {
+    const gameTbl = document.getElementById("gameTable");
+    const tbl = document.createElement('TABLE');
+    for (let i = 0; i<4; i++){
+      const tr=tbl.insertRow();
+
+      for(let j=0; j<10; j++){
+        const td= tr.insertCell();
+        const gamePlayBtn= document.createElement('button');
+        gamePlayBtn.setAttribute("class","gamePlayButton");
+        gamePlayBtn.setAttribute("id", `gamePlayBtn_${i}${j}`)
+        const gameBtnImage= document.createElement('img');
+        gameBtnImage.src = '/images/Merlionicon.png';
+        gameBtnImage.style.height='90px';
+        gamePlayBtn.appendChild(gameBtnImage);
+        td.appendChild(gamePlayBtn);
+        tr.appendChild(td);
+        gamePlayBtn.addEventListener('click', prizeWon)
+        
+      }
+    }
+    gameTbl.appendChild(tbl);
+    
+  };
+
+  //eventlistener
+  document.getElementById('game-btn').addEventListener('click',() =>{
+    document.getElementById('msgDisplay').innerText="Merlion Jackpot time"
+    document.getElementById('msgDisplay').style.backgroundColor='green';
+    createTable();
+    
+  }
+  );
+  const prizeWon = () => {
+    const randomID = Math.floor((Math.random()*40)+1);
+    for (const prizeType in prizes) {
+      if(prizes[prizeType].prizeID.includes(randomID)){
+        alert(`Congratulations. You have won ${prizes[prizeType].prizeInfo}`)
+        console.log(prizes[prizeType].prizeInfo);
+        console.log(randomID)
+      }
+    }
+  };
+  
+  // document.getElementByclassName('gamePlayButton').addEventListener('click',() => {
+  //   prizeWon();
+//   // });
+//   const gamePlayButtonSelected = document.getElementsByClassName('gamePlayButton');
+// console.log(gamePlayButtonSelected.length);
+//   for(let i = 0; i< gamePlayButtonSelected.length; i++){
+
+//     gamePlayButtonSelected[i].addEventListener('click',()=>{
+//       prizeWon();
+//     })
+//   }
